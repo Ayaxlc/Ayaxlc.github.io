5209c5bb0a341a0e43228cd7d283e978defec660
Ayaxlc <162739519+Ayaxlc@users.noreply.github.com>
2025-12-28 02:33:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    const darkSwitch = document.getElementById("darkSwitch");
+    const switchDark = document.getElementById("darkSwitch");
 
-    darkSwitch.addEventListener("change", () => {
-        document.body.classList.toggle("dark", darkSwitch.checked);
+    // Cargar preferencia
+    if (localStorage.getItem("theme") === "dark") {
+        document.body.classList.add("dark");
+        switchDark.checked = true;
+    }
+
+    switchDark.addEventListener("change", () => {
+        document.body.classList.toggle("dark");
+
+        localStorage.setItem(
+            "theme",
+            document.body.classList.contains("dark") ? "dark" : "light"
+        );
     });
 
+    // Botones credencial
     document.querySelectorAll(".btn-credential").forEach(btn => {
         btn.addEventListener("click", () => {
             window.open(btn.dataset.url, "_blank");
         });
     });
-
 });
