9f0b7d0d003be5fb4b12bd1c4169b4be2cc8f64c
Ayaxlc <162739519+Ayaxlc@users.noreply.github.com>
2026-01-04 19:12:50 +0100

    Create index.php

---
+++ proyectos/despliegue-aplicaciones-web/Despliegue-docker/src/index.php
@@ -0,0 +1,6 @@
+#Si consigo ver esto en el navegador es porque lo estamos ejecutando bien.
+<?php
+// phpinfo muestra información del servidor PHP
+// Se usa para comprobar que todo funciona correctamente
+
+phpinfo();
